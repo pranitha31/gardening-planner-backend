51c812e92e69c957a899293b4512b6a10202b4cf
pranitha <chikkelapranitha@gmail.com>
2026-02-28 21:23:27 +0530

    Initial backend setup

--- server.js
+++ server.js
@@ -55,3 +55,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Digital Time Capsule Backend is Running 🚀");
+});
